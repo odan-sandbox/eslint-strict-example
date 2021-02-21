d744621dbfbae6bfda1482add0ac32964f521f30
odan <odan3240@gmail.com>
2021-02-22 01:14:48 +0900

    add .eslintrc.strict.js

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ export function sum(values: number[]): number {
   return values.reduce((prev, cur): number => prev + cur, 0);
 }
 
-
 process.on("unhandledRejection", (reason) => {
   console.error(reason);
   process.exit(1);
